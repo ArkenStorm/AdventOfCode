65027f7e5566152f1d1abe369fc2987ce5d3d256
Taylor Whitlock <shadowarchon7@gmail.com>
2023-12-05 22:36:30 -0700

    Condensed code because fun python things

--- 2023/scripts/Day6 - Wait For It.py
+++ 2023/scripts/Day6 - Wait For It.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 inFile = open('../inputs/Day6.txt', 'r')
 lines = inFile.read().splitlines()
 
@@ -6,18 +8,11 @@ print('Day 6 - Wait For It')
 times = list(map(int, lines[0][5:].split()))
 distances = list(map(int, lines[1][9:].split()))
 
-def find_ways_to_beat_race(time, dist):
-	ways_to_beat_race = 0
-	for i in range(1, time):
-		if i * time - i > dist:
-			ways_to_beat_race += 1
-	return ways_to_beat_race
+def ways_to_beat_race(time, dist):
+	return sum(1 for i in range(1, time) if i* time - 1 > dist)
 
 # Part 1
-final = 1
-
-for i in range(len(times)):
-	final *= find_ways_to_beat_race(times[i], distances[i])
+final = reduce(lambda acc, idx: acc * ways_to_beat_race(times[idx], distances[idx]), range(len(times)), 1)
 
 print(f"Part 1: {final}")
 
@@ -25,6 +20,6 @@ print(f"Part 1: {final}")
 time = int(''.join(lines[0][5:].split()))
 dist = int(''.join(lines[1][9:].split()))
 
-ways_to_beat_race = find_ways_to_beat_race(time, dist)
+wins = ways_to_beat_race(time, dist)
 
-print(f"Part 2: {ways_to_beat_race}")
+print(f"Part 2: {wins}")
