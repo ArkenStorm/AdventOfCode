a7aa06940061f73be76d324f023e041c3c096c8c
Taylor Whitlock <taylor.whitlock@awardco.com>
2023-12-06 14:03:07 -0700

    typo op

--- 2023/scripts/Day6 - Wait For It.py
+++ 2023/scripts/Day6 - Wait For It.py
@@ -9,7 +9,7 @@ times = list(map(int, lines[0][5:].split()))
 distances = list(map(int, lines[1][9:].split()))
 
 def ways_to_beat_race(time, dist):
-	return sum(1 for i in range(1, time) if i* time - 1 > dist)
+	return sum(1 for i in range(1, time) if i * (time - i) > dist)
 
 # Part 1
 final = reduce(lambda acc, idx: acc * ways_to_beat_race(times[idx], distances[idx]), range(len(times)), 1)
