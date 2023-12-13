53e7d1bd80002aa78490ac3c4a36f31f124a1f73
Taylor Whitlock <taylor.whitlock@awardco.com>
2023-12-13 15:57:09 -0700

    Day 13 try to use numpy array slicing and numpy flip to check array equality

--- 2023/scripts/Day13 - Point of Incidence.py
+++ 2023/scripts/Day13 - Point of Incidence.py
@@ -4,8 +4,8 @@ from itertools import chain
 
 # inFile = open('../inputs/Day13.txt', 'r')
 # inFile = open('../inputs/test.txt', 'r')
-# inFile = open('2023/inputs/Day13.txt')
-inFile = open('2023/inputs/test.txt', 'r')
+inFile = open('2023/inputs/Day13.txt')
+# inFile = open('2023/inputs/test.txt', 'r')
 patterns = inFile.read().split('\n\n')
 
 print('Day 13 - Point of Incidence')
@@ -22,10 +22,12 @@ def check_reflection(pattern, pattern_idx, vertical: bool, smudges=0):
 			first, second = sequence[i], sequence[i+1]
 		else:
 			first, second = sequence[i][0], sequence[i+1][0]
-		if first == second:
+		diffs = 0
+		if first != second:
+			diffs += 1
+		if first == second or diffs <= smudges:
 			b, f = i, i + 1
 			is_mirrored = True
-			diffs = 0
 			while b >= 0 and f < len(sequence) and is_mirrored and diffs <= smudges:
 				if vertical:
 					b_slice, f_slice = pattern[:,b:b+1], pattern[:,f:f+1]
@@ -34,8 +36,8 @@ def check_reflection(pattern, pattern_idx, vertical: bool, smudges=0):
 				if not np.array_equal(b_slice, f_slice):
 					if smudges:
 						flat_b_slice, flat_f_slice = list(chain.from_iterable(b_slice)), list(chain.from_iterable(f_slice))
-						for i in range(len(flat_b_slice)):
-							if flat_b_slice[i] != flat_f_slice[i]:
+						for j in range(len(flat_b_slice)):
+							if flat_b_slice[j] != flat_f_slice[j]:
 								diffs += 1
 							if diffs > smudges:
 								is_mirrored = False
@@ -63,10 +65,44 @@ total = total_cols + (total_rows * 100)
 print(f"Part 1: {total}")
 
 # Part 2
+
+def check_reflection_2(pattern, pattern_idx, vertical: bool, smudges=0):
+	sequence = pattern[0] if vertical else pattern
+	for i in range(len(sequence) - 1):
+		if vertical:
+			first, second = sequence[i], sequence[i+1]
+		else:
+			first, second = sequence[i][0], sequence[i+1][0]
+		diffs = 0
+		if first != second:
+			diffs += 1
+		if first == second or diffs <= smudges:
+			is_mirrored = True
+
+			b_bound, f_bound = max(0, i+1), min(len(sequence), i+1)
+			if vertical:
+				b_arr, f_arr = pattern[:,:i+1], np.fliplr(pattern[:,i+1:])
+			else:
+				b_arr, f_arr = pattern[:i+1,:], np.flipud(pattern[i+1:,:])
+			diffs = np.sum(b_arr == f_arr)
+			
+			
+			if is_mirrored:
+				val = (i, vertical)
+				if pattern_idx not in reflections or reflections[pattern_idx] != val:
+					reflections[pattern_idx] = (i, vertical)
+					return i + 1
+				else:
+					continue
+	return 0
+
+
+
+
 total_cols, total_rows = 0, 0
 for idx, pattern in enumerate(patterns):
-	total_cols += check_reflection(pattern, idx, True, 1)
-	total_rows += check_reflection(pattern, idx, False, 1)
+	total_cols += check_reflection_2(pattern, idx, True, 1)
+	total_rows += check_reflection_2(pattern, idx, False, 1)
 
 total = total_cols + (total_rows * 100)
 print(f"Part 2: {total}")
