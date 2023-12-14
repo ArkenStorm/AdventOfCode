0b4df2c5bdcff4e211cb2de885105ec2f3157912
Taylor Whitlock <taylor.whitlock@awardco.com>
2023-12-14 16:22:05 -0700

    Day 14 changes and continued work

--- 2023/scripts/Day14 - Parabolic Reflector Dish.py
+++ 2023/scripts/Day14 - Parabolic Reflector Dish.py
@@ -13,29 +13,32 @@ platform = np.array([list(line) for line in lines])
 print('Day 14 - Parabolic Reflector Dish')
 
 def tilt(grid):
-	load = 0
 	for i in range(len(lines[0])):
 		col = ''.join(chain.from_iterable(grid[:,i:i+1])).split('#')
 		for j in range(len(col)):
 			col[j] = ''.join(sorted(col[j])[::-1])
-		sorted_col = '#'.join(col)
 		
+		sorted_col = '#'.join(col)	
 		grid[:,i:i+1] = np.array(list([r] for r in sorted_col))
-		for k in range(len(sorted_col)):
-			if sorted_col[k] == 'O':
-				load += len(sorted_col) - k
-	return load, np.rot90(grid, axes=(1,0))
+	return grid
+
+def get_load(grid):
+	return sum(len(grid) - i if grid[i][j] == 'O' else 0 for j in range(len(grid[0])) for i in range(len(grid)))
 
 # Part 1
-total, new_grid = tilt(platform)
+new_grid = tilt(platform)
+total = get_load(new_grid)
 
 print(f"Part 1: {total}")
 
 # Part 2
-states = { np.array2string(new_grid): 1 }  # grid state: iteration
+states = { np.array2string(platform): 0 }  # grid state: iteration
+new_grid = platform
 cycle_start, period = 0, 0
-for i in range(1, 1000000000):
-	total, new_grid = tilt(new_grid)
+for i in range(1000000000):
+	for j in range(4):
+		new_grid = tilt(np.rot90(new_grid, axes=(1,0)))
+	total = get_load(new_grid)
 	grid_str = np.array2string(new_grid)
 	if grid_str in states:
 		cycle_start = states[grid_str]
@@ -45,7 +48,9 @@ for i in range(1, 1000000000):
 		states[grid_str] = i
 
 for i in range(cycle_start + (1000000000 - cycle_start) % period):
-	total, new_grid = tilt(new_grid)
-	print(total)
+	for j in range(4):
+		new_grid = tilt(np.rot90(new_grid, axes=(1,0)))
 
-print(total)
+total = get_load(new_grid)
+
+print(f"Part 2: {total}")
