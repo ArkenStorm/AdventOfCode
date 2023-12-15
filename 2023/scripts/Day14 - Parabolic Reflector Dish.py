30f06363daba1e1ff8a5925ae236eafb0ae4f2d9
Taylor Whitlock <shadowarchon7@gmail.com>
2023-12-14 20:21:46 -0700

    Day 14 part 2 done...dadgum numpy array2string

--- 2023/scripts/Day14 - Parabolic Reflector Dish.py
+++ 2023/scripts/Day14 - Parabolic Reflector Dish.py
@@ -4,20 +4,24 @@ from itertools import chain
 
 # inFile = open('../inputs/Day14.txt', 'r')
 # inFile = open('../inputs/test.txt', 'r')
-# inFile = open('2023/inputs/Day14.txt')
-inFile = open('2023/inputs/test.txt', 'r')
+inFile = open('2023/inputs/Day14.txt')
+# inFile = open('2023/inputs/test.txt', 'r')
 lines = inFile.read().splitlines()
+
+print('Day 14 - Parabolic Reflector Dish')
 
 platform = np.array([list(line) for line in lines])
 
-print('Day 14 - Parabolic Reflector Dish')
+def grid_str(grid: np.ndarray):
+	return '\n'.join([''.join(r) for r in grid.tolist()])
+
+states = { grid_str(platform): 0 }  # grid state: iteration
 
 def tilt(grid):
 	for i in range(len(lines[0])):
 		col = ''.join(chain.from_iterable(grid[:,i:i+1])).split('#')
 		for j in range(len(col)):
 			col[j] = ''.join(sorted(col[j])[::-1])
-		
 		sorted_col = '#'.join(col)	
 		grid[:,i:i+1] = np.array(list([r] for r in sorted_col))
 	return grid
@@ -26,31 +30,33 @@ def get_load(grid):
 	return sum(len(grid) - i if grid[i][j] == 'O' else 0 for j in range(len(grid[0])) for i in range(len(grid)))
 
 # Part 1
-new_grid = tilt(platform)
-total = get_load(new_grid)
+tilt(platform)
+total = get_load(platform)
 
 print(f"Part 1: {total}")
 
 # Part 2
-states = { np.array2string(platform): 0 }  # grid state: iteration
-new_grid = platform
+def cycle(grid):
+	for _ in range(4):
+		tilt(grid)
+		grid = np.rot90(grid, axes=(1,0))
+	return grid
+
 cycle_start, period = 0, 0
-for i in range(1000000000):
-	for j in range(4):
-		new_grid = tilt(np.rot90(new_grid, axes=(1,0)))
-	total = get_load(new_grid)
-	grid_str = np.array2string(new_grid)
-	if grid_str in states:
-		cycle_start = states[grid_str]
-		period = i - cycle_start - 1
+for i in range(1, 1000000000):
+	platform = cycle(platform)
+	total = get_load(platform)
+	key = grid_str(platform)
+	if key in states:
+		cycle_start = states[key]
+		period = i - cycle_start
 		break
 	else:
-		states[grid_str] = i
+		states[key] = i
 
-for i in range(cycle_start + (1000000000 - cycle_start) % period):
-	for j in range(4):
-		new_grid = tilt(np.rot90(new_grid, axes=(1,0)))
+final = list(states.keys())[list(states.values()).index(cycle_start + (1000000000 - cycle_start) % period)].split()
+platform = np.array([list(r) for r in final])
 
-total = get_load(new_grid)
+total = get_load(platform)
 
 print(f"Part 2: {total}")
