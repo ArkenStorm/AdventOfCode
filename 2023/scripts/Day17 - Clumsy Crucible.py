7f778427e229c45076dbf26056cdf7a153bbad22
Taylor Whitlock <shadowarchon7@gmail.com>
2023-12-17 10:48:20 -0700

    Day17 part 1 and 2 done. sooo much debugging, yeesh

---
+++ 2023/scripts/Day17 - Clumsy Crucible.py
@@ -0,0 +1,83 @@
+from functools import reduce
+import heapq as hq
+import sys
+sys.setrecursionlimit(10000)
+
+inFile = open('../inputs/Day17.txt', 'r')
+# inFile = open('../inputs/test.txt', 'r')
+# inFile = open('2023/inputs/Day17.txt')
+# inFile = open('2023/inputs/test.txt', 'r')
+lines = inFile.read().splitlines()
+
+grid = [list(map(int, line)) for line in lines]
+n, e, s, w = (-1, 0), (0, 1), (1, 0), (0, -1)
+dirs = [n, e, s, w]
+inverses = { n: s, e: w, s: n, w: e }
+
+print('Day 17 - Clumsy Crucible')
+
+# Part 1
+
+# def get_possible_dirs(pos: tuple, curr_dr: tuple, streak: int, path: set):
+# 	bounded_dirs = list(filter(
+# 		lambda d: 0 <= pos[0] + d[0] < len(grid) and 
+# 			0 <= pos[1] + d[1] < len(grid[0]) and 
+# 			d != inverses[curr_dr] and 
+# 			((pos[0] + d[0], pos[1] + d[1]), curr_dr) not in path,
+# 		dirs
+# 	))
+# 	return bounded_dirs if streak < 3 else list(filter(lambda d: d != curr_dr, bounded_dirs))
+
+
+min_heat_loss = float('inf')
+
+queue = [(0, 1, (0,0), e), (0, 1, (0,0), s)]
+hq.heapify(queue)
+
+def dijkstra(q: list, min_streak=1, max_streak=3):
+	global min_heat_loss
+
+	path = set()
+	while len(q) > 0:
+		curr_loss, streak, pos, dir = hq.heappop(q)
+		if (pos, dir, streak) in path:
+			continue
+		path.add((pos, dir, streak))
+		# possibles = get_possible_dirs(pos, dir, streak, path)
+		# for new_dir in possibles:
+		# 	new_pos = (pos[0] + new_dir[0], pos[1] + new_dir[1])
+		# 	new_streak = streak + 1 if new_dir == dir else 1
+		# 	new_loss = curr_loss + grid[new_pos[0]][new_pos[1]]
+		# 	if new_pos == (len(grid) - 1, len(grid[0]) - 1):
+		# 		min_heat_loss = min(min_heat_loss, new_loss)
+		# 	hq.heappush(q, (new_loss, new_streak, new_pos, new_dir))
+
+		for new_dir in dirs:
+			new_pos = (pos[0] + new_dir[0], pos[1] + new_dir[1])
+			new_streak = streak + 1 if new_dir == dir else 1
+			if (
+				not 0 <= new_pos[0] < len(grid) or 
+				not 0 <= new_pos[1] < len(grid[0]) or 
+				new_dir == inverses[dir] or
+				(new_dir != dir and streak < min_streak) or
+				(new_dir == dir and streak == max_streak) or
+				(new_pos, new_dir, new_streak) in path
+			):
+				continue
+			new_loss = curr_loss + grid[new_pos[0]][new_pos[1]]
+			if new_pos == (len(grid) - 1, len(grid[0]) - 1):
+				min_heat_loss = min(min_heat_loss, new_loss)
+			hq.heappush(q, (new_loss, new_streak, new_pos, new_dir))
+
+dijkstra(queue)
+
+print(f"Part 1: {min_heat_loss}")
+
+# Part 2
+queue = [(0, 1, (0,0), e), (0, 1, (0,0), s)]
+hq.heapify(queue)
+min_heat_loss = float('inf')
+
+dijkstra(queue, 4, 10)
+
+print(f"Part 2: {min_heat_loss}")
