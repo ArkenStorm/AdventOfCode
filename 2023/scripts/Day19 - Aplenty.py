fff429f6c949f6b40f3cf3f8abcca27af7c11f84
Taylor Whitlock <shadowarchon7@gmail.com>
2023-12-19 11:29:39 -0700

    Day 19 part 2 was a doozy, but I'm proud of it!

--- 2023/scripts/Day19 - Aplenty.py
+++ 2023/scripts/Day19 - Aplenty.py
@@ -1,17 +1,13 @@
 import re
 from functools import reduce
-# import operator
-# import sys
-# sys.setrecursionlimit(10000)
 
-# inFile = open('../inputs/Day19.txt', 'r')
+inFile = open('../inputs/Day19.txt', 'r')
 # inFile = open('../inputs/test.txt', 'r')
 # inFile = open('2023/inputs/Day19.txt')
-inFile = open('2023/inputs/test.txt', 'r')
+# inFile = open('2023/inputs/test.txt', 'r')
 rules, parts = inFile.read().split('\n\n')
 rules, parts = rules.split('\n'), parts.split('\n')
 
-# ops = { '<': operator.lt, '>': operator.gt }
 ops = { '<': lambda a, b: a < b, '>': lambda a, b: a > b }
 
 print('Day 19 - Aplenty')
@@ -36,17 +32,14 @@ for rule in rules:
 
 
 # Part 1
-accepted, acc_set = [], set()
+accepted = []
 
 def send_to_dest(part, dest):
-	part_str = str(part)
 	if dest == 'A':
-		if part_str not in acc_set:
-			accepted.append(part)
-			acc_set.add(part_str)
-			return
+		accepted.append(part)
+		return
 	elif dest == 'R':
-			return
+		return
 	else:
 		return process_part(part, dest)
 
@@ -73,3 +66,40 @@ accepted_ratings = reduce(lambda acc, p: acc + sum(p.values()), accepted, 0)
 print(f"Part 1: {accepted_ratings}")
 
 # Part 2
+ranges = { 'x': [1, 4000], 'm': [1, 4000], 'a': [1, 4000], 's': [1, 4000] }
+combos = 0
+
+def handle_dest(dest, r: dict):
+	global combos
+
+	if dest == 'A':
+		combos += reduce(lambda acc, p: acc * (abs(r[p][1] - r[p][0]) + 1), r, 1)
+		return
+	elif dest == 'R':
+		return
+	else:
+		return find_ranges(dest, r)
+
+# r is the current working range
+def find_ranges(wf, r: dict):
+	conditions = wfs[wf]
+	for cond in conditions:
+		if len(cond) == 1:
+			return handle_dest(cond[0], r)
+		else:
+			# split range at val
+			prop, op, val, dest = cond
+			pass_copy = r.copy()
+			if op == '<':
+				pass_copy[prop] = [r[prop][0], val - 1]
+				r[prop] = [val, r[prop][1]]
+			else:
+				pass_copy[prop] = [val + 1, r[prop][1]]
+				r[prop] = [r[prop][0], val]
+			handle_dest(dest, pass_copy)
+			# continue with current r
+
+find_ranges('in', ranges)
+
+
+print(f"Part 2: {combos}")
