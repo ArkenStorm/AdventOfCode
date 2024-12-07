e754fd073bb11436e16157743b3fa81e154ddb46
Taylor Whitlock <shadowarchon7@gmail.com>
2024-12-07 12:22:16 -0700

    Day 7 done

---
+++ 2024/scripts/Day7.py
@@ -0,0 +1,52 @@
+from utils import *
+from datetime import datetime
+from math import log10
+from functools import reduce
+from operator import add, mul
+
+def concat_ints(x, y): # stolen from reddit for optimization
+    return x * (10 ** (int(log10(y)) + 1)) + y  # int(str(x) + str(y))
+
+def is_valid_eq(target, operands, operators):
+	reductor = lambda acc, n: [res for partial in acc for op in operators if (res := op(partial, n)) <= target]
+	results = reduce(reductor, operands[1:], [operands[0]])
+	return target in results
+
+def part_1(input):
+	total = 0
+
+	for eq in input:
+		target, operands = eq.split(':')
+		target = int(target)
+		operands = list(map(int, operands.split()))
+
+		if is_valid_eq(target, operands, [add, mul]):
+			total += target
+	return total
+
+def part_2(input):
+	total = 0
+
+	for eq in input:
+		target, operands = eq.split(':')
+		target = int(target)
+		operands = list(map(int, operands.split()))
+
+		if is_valid_eq(target, operands, [add, mul, concat_ints]):
+			total += target
+	return total
+
+def main(year, day):
+	print(f'Day {day} - Bridge Repair')
+
+	input = get_input(year, day, test=False)
+
+	p1_start = datetime.now()
+	print(f'Part 1: {part_1(input)}')
+	p1_end = datetime.now()
+
+	p2_start = datetime.now()
+	print(f'Part 2: {part_2(input)}')
+	p2_end = datetime.now()
+
+	print(f'\nExecution Times:\n\tPart 1: {p1_end - p1_start}\n\tPart 2: {p2_end - p2_start}\n')
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 if __name__ == '__main__':
 	year = 2024
-	day = 6
+	day = 7
 
 	print(f'Advent of Code {year}')
 
