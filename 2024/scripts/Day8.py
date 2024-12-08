6376ddd4531eaee131b85f316255055b7b4e54de
Taylor Whitlock <shadowarchon7@gmail.com>
2024-12-07 23:55:47 -0700

    cleanup on aisle 8

--- 2024/scripts/Day8.py
+++ 2024/scripts/Day8.py
@@ -7,42 +7,31 @@ def in_bounds(grid, x, y):
 	return 0 <= x < len(grid) and 0 <= y < len(grid[0])
 
 def part_1(grid, node_coords):
-	antinode_coords = set()
+    antinode_coords = set()
 
-	for key in node_coords:
-		coord_pairs = [sorted((a, b)) for idx, a in enumerate(node_coords[key]) for b in node_coords[key][idx + 1:]]
-		for pair in coord_pairs:
-			diff = (pair[1][0] - pair[0][0], pair[1][1] - pair[0][1])
-			a1 = (pair[0][0] - diff[0], pair[0][1] - diff[1])
-			a2 = (pair[1][0] + diff[0], pair[1][1] + diff[1])
-			if in_bounds(grid, *a1):
-				antinode_coords.add(a1)
-			if in_bounds(grid, *a2):
-				antinode_coords.add(a2)
-	return len(antinode_coords)
+    for nodes in (node_coords.values()):
+        for (a, b) in nodes:
+            diff = (b[0] - a[0], b[1] - a[1])
+            neighbors = [(a[0] - diff[0], a[1] - diff[1]), (b[0] + diff[0], b[1] + diff[1])]
+
+            antinode_coords.update(coord for coord in neighbors if in_bounds(grid, *coord))
+
+    return len(antinode_coords)
+
+def add_vector_nodes(start, step, coords, grid):
+	curr = start
+	while in_bounds(grid, *curr):
+		coords.add(curr)
+		curr = (curr[0] + step[0], curr[1] + step[1])
 
 def part_2(grid, node_coords):
 	antinode_coords = set()
 
-	for key in node_coords:
-		coord_pairs = [sorted((a, b)) for idx, a in enumerate(node_coords[key]) for b in node_coords[key][idx + 1:]]
-		for pair in coord_pairs:
+	for nodes in (node_coords.values()):
+		for pair in nodes:
 			diff = (pair[1][0] - pair[0][0], pair[1][1] - pair[0][1])
-
-			curr = pair[0]
-			while True:
-				if not in_bounds(grid, *curr):
-					break
-				antinode_coords.add(curr)
-				curr = (curr[0] - diff[0], curr[1] - diff[1])
-
-			curr = pair[1]
-			while True:
-				if not in_bounds(grid, *curr):
-					break
-				antinode_coords.add(curr)
-				curr = (curr[0] + diff[0], curr[1] + diff[1])
-
+			add_vector_nodes(pair[0], (-diff[0], -diff[1]), antinode_coords, grid)
+			add_vector_nodes(pair[1], diff, antinode_coords, grid)
 	return len(antinode_coords)
 
 def main(year, day):
@@ -50,13 +39,16 @@ def main(year, day):
 
 	input = get_input(year, day, type='grid', test=False)
 
-	node_coords = defaultdict(list)
 	grid = np.array(input)
+	node_coords = defaultdict(list)
 
 	for i in range(grid.shape[0]):
 		for j in range(grid.shape[1]):
 			if input[i][j] != '.':
 				node_coords[input[i][j]].append((i, j))
+
+	for key, nodes in node_coords.items():
+		node_coords[key] = [sorted((a, b)) for idx, a in enumerate(nodes) for b in nodes[idx + 1:]]
 
 	p1_start = datetime.now()
 	print(f'Part 1: {part_1(grid, node_coords)}')
