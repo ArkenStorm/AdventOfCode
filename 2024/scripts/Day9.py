7d6becc910049b4d0f842bfe18e84f8b72c0386f
Taylor Whitlock <shadowarchon7@gmail.com>
2024-12-09 21:53:32 -0700

    Day 9 part 2 done

--- 2024/scripts/Day9.py
+++ 2024/scripts/Day9.py
@@ -15,40 +15,33 @@ def part_1(system_map):
 	return sum(int(val) * i for i, val in enumerate(filtered_system_map))
 
 def part_2(system_map, id_to_size):
-	start, end = 0, len(system_map) - 1
-	while system_map[end] == '.': end -= 1
+	og_file_pos = file_pos = len(system_map) - 1
+	for id in list(id_to_size.keys())[::-1]:
+		disk_pos = 0
+		end_size = id_to_size[id]
 
-	# start at the end, find the first block of free space that fits it, starting at the beginning every time.
+		file_pos = og_file_pos
+		while system_map[file_pos] != id: file_pos -= 1
+		og_file_pos = file_pos - (end_size - 1)
 
-	defragged = []
-	while start < end:
-		end_size = id_to_size[system_map[end]]
-		free_block = start + end_size
-		if system_map[start:free_block] == ['.'] * end_size:
-			defragged += system_map[end - end_size + 1:end + 1]
-			start += end_size
-			end -= end_size
-			while system_map[end] == '.': end -= 1
-		elif system_map[start] == '.':
-			while system_map[start] == '.':
-				defragged += system_map[start]
-				start += 1
-		else:
-			block_end = start + id_to_size[system_map[start]]
-			defragged += system_map[start:block_end]
-			start += id_to_size[system_map[start]]
+		free_block = disk_pos + end_size
+		while system_map[disk_pos:free_block] != ['.'] * end_size and disk_pos < og_file_pos:
+			disk_pos += 1
+			free_block = disk_pos + end_size
+		if disk_pos < og_file_pos:
+			for _ in range(end_size):
+				system_map[disk_pos], system_map[file_pos] = system_map[file_pos], system_map[disk_pos]
+				disk_pos += 1
+				file_pos -= 1
 
-	return sum(int(val) * i for i, val in enumerate(defragged) if val != '.')
+	return sum(int(val) * i for i, val in enumerate(system_map) if val != '.')
 
 def main(year, day):
 	print(f'Day {day} - Disk Fragmenter')
 
-	input = get_input(year, day, test=True)
+	input = get_input(year, day, test=False)
 
 	id_to_size = { str(id): int(size) for id, size in enumerate(input[0][::2]) }
-	# print(id_to_size)
-	# free_spaces = input[1::2]
-
 
 	system_map = []
 	for id, size in enumerate(input[0]):
