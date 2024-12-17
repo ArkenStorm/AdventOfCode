68690f1b58ae399623702d0a36cf28ae8ebb7778
Taylor Whitlock <shadowarchon7@gmail.com>
2024-12-17 00:14:59 -0700

    Day 17 done

--- 2024/scripts/Day17.py
+++ 2024/scripts/Day17.py
@@ -3,65 +3,42 @@ from datetime import datetime
 import re
 
 registers = { 'a': 0, 'b': 0, 'c': 0 }
-combo_ops = {
-	0: 0,
-	1: 1,
-	2: 2,
-	3: 3,
-	4: lambda: registers['a'],
-	5: lambda: registers['b'],
-	6: lambda: registers['c'],
-	7: lambda: print('Reserved, program invalid')
-}
+combo_ops = { 0: 0, 1: 1, 2: 2, 3: 3, 4: lambda: registers['a'], 5: lambda: registers['b'], 6: lambda: registers['c'], 7: lambda: print('Reserved, program invalid') }
 
 def get_combo_op(operand):
 	return combo_ops[operand] if type(combo_ops[operand]) == int else combo_ops[operand]()
 
-def adv(operand):
-	registers['a'] //= 2**get_combo_op(operand)
-
-def bxl(operand):
-	registers['b'] ^= operand
-
-def bst(operand):
-	registers['b'] = get_combo_op(operand) % 8
-
-# set iptr to return value of jnz if not none
-def jnz(operand):
-	return operand if registers['a'] != 0 else None
-
-def bxc(_):
-	registers['b'] = registers['b'] ^ registers['c']
-
-def out(operand, output: list):
-	output.append(get_combo_op(operand) % 8)
-
-def bdv(operand):
-	registers['b'] = registers['a'] // 2**get_combo_op(operand)
-
-def cdv(operand):
-	registers['c'] = registers['a'] // 2**get_combo_op(operand)
-
-def part_1(program):
+def run_program(program):
 	iptr = 0
 	output = []
 	while iptr < len(program):
 		op, operand = program[iptr], program[iptr + 1]
 		ret = None
-		if op == 0: adv(operand)
-		elif op == 1: bxl(operand)
-		elif op == 2: bst(operand)
-		elif op == 3: ret = jnz(operand)
-		elif op == 4: bxc(operand)
-		elif op == 5: out(operand, output)
-		elif op == 6: bdv(operand)
-		elif op == 7: cdv(operand)
+		if op == 0: registers['a'] //= 2**get_combo_op(operand) # adv
+		elif op == 1: registers['b'] ^= operand # bxl
+		elif op == 2: registers['b'] = get_combo_op(operand) % 8 # bst
+		elif op == 3: ret = operand if registers['a'] != 0 else None # jnz
+		elif op == 4: registers['b'] = registers['b'] ^ registers['c'] # bxc
+		elif op == 5: output.append(get_combo_op(operand) % 8) # out
+		elif op == 6: registers['b'] = registers['a'] // 2**get_combo_op(operand) # bdv
+		elif op == 7: registers['c'] = registers['a'] // 2**get_combo_op(operand) # cdv
 		iptr = ret if ret is not None else iptr + 2
-	return ','.join(map(str, output))
+	return output
+
+def part_1(program):
+	return ','.join(map(str, run_program(program)))
 
 def part_2(program):
-	# track all operations and values at each step from part 1?
-	pass
+	q = [(len(program) - 1, 0)] # program always ends with zero
+	while len(q) > 0:
+		i, opcode = q.pop(0)
+		for op in range(8):
+			reg_val = (opcode << 3) + op
+			registers['a'] = reg_val
+			if run_program(program) == program[i:]:
+				if i == 0: return reg_val
+				q.append((i - 1, reg_val))
+	return None
 
 def main(year, day):
 	print(f'Day {day} - Chronospatial Computer')
